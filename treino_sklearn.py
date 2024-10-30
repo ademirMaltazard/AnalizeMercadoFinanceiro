f7e0e64c76ecf31b74b4b21881f2bd8da3709cf2
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-10-30 23:52:41 +0000

    teste do programa ultilizando sklearn com base de teste

--- treino_sklearn.py
+++ treino_sklearn.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
 
 entradas_or = np.array([[0, 0], [0, 1], [1, 0],[1, 1]])
 saidas_or = np.array([0, 1, 1, 1])
 
 perceptron = Perceptron(max_iter=100, eta0=0.1)
 perceptron.fit(entradas_or, saidas_or)
-print('+++')
 
 print("Pesos: ", perceptron.coef_)
 print("bias: ", perceptron.intercept_)
@@ -16,3 +16,17 @@ teste = np.array([[0, 1]])
 
 previsao = perceptron.predict(teste)
 print(previsao)
+
+dados = pd.read_csv("data_banknote_authentication.txt", header = None)
+x_dados = dados.iloc[:, 0: 4].values
+y_dados = dados.iloc[:, 4].values
+
+perceptron.fit(x_dados, y_dados)
+
+print("Pesos: ", perceptron.coef_)
+print("bias: ", perceptron.intercept_)
+
+previsao = perceptron.predict(x_dados)
+acuracia = accuracy_score(y_dados, previsao)
+
+print(acuracia)
