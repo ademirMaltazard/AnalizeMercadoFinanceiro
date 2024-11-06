864b9bdd5923bac8fc4a1cdf6bff579f0cab4877
Ademir Gama <159483317+ademirMaltazard@users.noreply.github.com>
2024-11-06 23:38:14 +0000

    inicio de codico para analize do censo

---
+++ AnalizeDoCenso/censu.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+base_censu = pd.read_csv('AnalizeDoCenso/census.csv')
+print(base_censu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
